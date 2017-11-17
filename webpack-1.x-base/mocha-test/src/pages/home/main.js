b78771d8c50a28742c8a8f46c7ba88814353c922
paygle <dodar@qq.com>
2017-11-17 10:39:31 +0800

    add environment

--- webpack-1.x-base/mocha-test/src/pages/home/main.js
+++ webpack-1.x-base/mocha-test/src/pages/home/main.js
@@ -1,11 +1,15 @@
 
 import './main.css';
 import Vue from 'vue';
+import Hello from 'packages/hello-world'
 
 new Vue({
 	el: '#page-main',
+	components: {
+		Hello
+	},
 	data: {
-	
+
 	},
 	methods: {
 
