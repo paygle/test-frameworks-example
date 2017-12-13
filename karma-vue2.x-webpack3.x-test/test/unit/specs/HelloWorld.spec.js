0c8bcc9a27f02a910bb39861e431dc653696e7fa
paygle <dodar@qq.com>
2017-12-13 10:24:21 +0800

    修改目录

--- karma-vue2.x-webpack3.x-test/test/unit/specs/HelloWorld.spec.js
+++ karma-vue2.x-webpack3.x-test/test/unit/specs/HelloWorld.spec.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import { Button, Select, Option } from 'element-ui';
-import HelloWorld from '@/packages/HelloWorld';
+import HelloWorld from '@/HelloWorld';
 
 Vue.use(Button);
 Vue.use(Select);
